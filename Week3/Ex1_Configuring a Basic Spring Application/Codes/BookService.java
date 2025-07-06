530d58de5cb0baa90393329d4721d336a4aec747
Thammineni Mohan Raj Kumar <116945231+Mohan2200031661@users.noreply.github.com>
2025-07-06 20:18:11 +0530

    Hands on Spring Core Maven

---
+++ Week3/Ex1_Configuring a Basic Spring Application/Codes/BookService.java
@@ -0,0 +1,9 @@
+package com.library.service;
+public class BookService {
+    public BookService() {
+        System.out.println("[Bean Initialized] BookService instantiated.");
+    }
+    public void displayBooks() {
+        System.out.println("[Bean Method] BookService: Displaying books.");
+    }
+}
