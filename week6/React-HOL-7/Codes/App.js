d3afdb9a9709e11b7c29d6e79c328d2bb9997a42
Thammineni Mohan Raj Kumar <116945231+Mohan2200031661@users.noreply.github.com>
2025-07-27 20:48:56 +0530

    React Hands On Implementation

---
+++ week6/React-HOL-7/Codes/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import OnlineShopping from './OnllineShopping';
+
+function App() {
+  return (
+      <div className="App">
+        <OnlineShopping />
+      </div>
+  );
+}
+
+export default App;
