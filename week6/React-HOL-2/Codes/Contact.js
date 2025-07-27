9cc5de83a8c0c8d45c943dbd7506e06e5cb45d53
Thammineni Mohan Raj Kumar <116945231+Mohan2200031661@users.noreply.github.com>
2025-07-27 17:35:45 +0530

    React Hands on Learning

---
+++ week6/React-HOL-2/Codes/Contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+    render() {
+        return (
+            <div className="contact">
+                <h1>Welcome to the Contact Page of Student Managent Portal</h1>
+            </div>
+        );
+    }
+}
+
+export default Contact;
---
+++ week6/React-HOL-4/Codes/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Posts from './Posts';
+
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Blog Posts</h1>
+        </header>
+        <Posts />
+      </div>
+  );
+}
+
+export default App;
